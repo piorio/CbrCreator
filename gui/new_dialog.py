7b3ba6522c4313a12ee0e382841d5f59ff062e3d
pablo <iorio.paolo@gmail.com>
2014-08-28 10:38:00 +0200

    On new dialog the OK Button is enabled/Disabled based on the value inserted by user

--- gui/new_dialog.py
+++ gui/new_dialog.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QPushButton
 from gui.ui_NewDialog import Ui_Dialog
 
 __author__ = 'pablo'
@@ -8,6 +9,32 @@ class NewDialog(QDialog):
     def __init__(self):
         super(NewDialog, self).__init__()
 
+        self.url = None
+        self.destination = None
+
         # Set up the user interface from Designer.
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+
+        #Disable the OK button
+        self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
+
+        #Connect
+        self.ui.url_line_edit.textEdited.connect(self.on_url_line_edit_finish)
+        self.ui.destination_line_edit.textEdited.connect(self.on_destination_line_edit_finish)
+
+    @pyqtSlot(str)
+    def on_url_line_edit_finish(self, text):
+        self.url = self.ui.url_line_edit.text()
+        if self.url is not None and self.destination is not None and len(self.url) > 0 and len(self.destination) > 0:
+            self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
+        else:
+            self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
+
+    @pyqtSlot(str)
+    def on_destination_line_edit_finish(self, text):
+        self.destination = self.ui.destination_line_edit.text()
+        if self.url is not None and self.destination is not None and len(self.url) > 0 and len(self.destination) > 0:
+            self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
+        else:
+            self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
