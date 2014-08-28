691d3a90f80b272d91d852655859f829a4390377
pablo <iorio.paolo@gmail.com>
2014-08-28 11:51:22 +0200

    Design the Main Window with tool bar. Fix some open points

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
 from gui.new_dialog import NewDialog
 from gui.ui_MainWindow import Ui_MainWindow
 
@@ -17,6 +17,10 @@ class MainWindow(QMainWindow):
         #Connect
         self.ui.action_Quit.triggered.connect(self.quit)
         self.ui.action_New.triggered.connect(self.on_new)
+        self.ui.actionDownload_chapters.triggered.connect(self.on_download_chapters_triggered)
+
+        #Setup default value
+        self.ui.chapters_table_widget.horizontalHeader().setStretchLastSection(True)
 
     @pyqtSlot()
     def quit(self):
@@ -25,4 +29,10 @@ class MainWindow(QMainWindow):
     @pyqtSlot()
     def on_new(self):
         new_dialog = NewDialog()
-        new_dialog.exec()
+        if new_dialog.exec() == QDialog.Accepted:
+            self.ui.actionDownload_chapters.setEnabled(True)
+            self.ui.url_line_edit.setText(new_dialog.url)
+
+
+    def on_download_chapters_triggered(self):
+        print('Download')
